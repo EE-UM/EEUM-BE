09f3be0ceb634035b56b0a29568d75478aed9458
LeeJaeHyeok97 <hazardous10@naver.com>
2025-06-15 00:02:31 +0900

    chore: 멀티모듈 프로젝트 설정

--- service/user/src/main/java/com/eeum/UserApplication.java
+++ service/user/src/main/java/com/eeum/UserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EeumApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EeumApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
