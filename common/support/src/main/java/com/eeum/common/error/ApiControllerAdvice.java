5ee809a296f477919c9edea5ebbdd0a685ed56df
LeeJaeHyeok97 <hazardous10@naver.com>
2025-06-17 14:59:58 +0900

    refacotr: 전역예외처리기 공통 모듈화

--- common/support/src/main/java/com/eeum/common/error/ApiControllerAdvice.java
+++ common/support/src/main/java/com/eeum/common/error/ApiControllerAdvice.java
@@ -1,7 +1,5 @@
-package com.eeum.user.config.error;
+package com.eeum.common.error;
 
-import com.eeum.common.error.CoreApiException;
-import com.eeum.common.error.ErrorType;
 import com.eeum.common.response.ApiResponse;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @io.swagger.v3.oas.annotations.Hidden
 public class ApiControllerAdvice {
+
     @ExceptionHandler
     public ResponseEntity<ApiResponse<?>> handleCoreApiException(CoreApiException e) {
         return new ResponseEntity(ApiResponse.error(e.getErrorType(), e.getData()), HttpStatusCode.valueOf(e.getErrorType().getStatusCode()));
