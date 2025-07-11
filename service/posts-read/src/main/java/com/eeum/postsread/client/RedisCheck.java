12f0f3752a896730aae43f5a6882e7dcb0f9a9db
이재혁 <hazardous10@naver.com>
2025-07-11 16:46:34 +0900

    feat: 홈화면 캐싱 개선 및 Redis TTL 설정 도입 (#8)
    * feat: postsclient 요청 코드 추가
    
    * feat: post-read 서버 조회 기능 구현
    
    * feat: redis용 dataserializer 추가
    
    * feat: 게시글 내용 redis 캐싱 추가
    
    * feat: 게시글 무한 스크롤 구현
    
    * feat: 게시글 목록 조회 시 redis 캐시 적용
    
    * feat: 홈화면 캐싱 ttl 24h -> 60sec 변경

--- service/posts-read/src/main/java/com/eeum/postsread/client/RedisCheck.java
+++ service/posts-read/src/main/java/com/eeum/postsread/client/RedisCheck.java
@@ -14,8 +14,15 @@ public class RedisCheck {
     @PostConstruct
     public void testRedis() {
         try {
-            redisTemplate.opsForValue().set("redis-check", "true");
-            System.out.println("✅ Redis is working");
+            // 값 조회
+            String value = redisTemplate.opsForValue().get("redis-check");
+
+            // 로그 출력
+            if (value != null) {
+                System.out.println("✅ Redis is working. Value of 'redis-check': " + value);
+            } else {
+                System.out.println("⚠️ Redis connected, but value not found.");
+            }
         } catch (Exception e) {
             System.err.println("❌ Redis connection failed: " + e.getMessage());
         }
