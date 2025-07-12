361d3e0635b55a9d5e32240bc654459d55a56bb1
LeeJaeHyeok97 <hazardous10@naver.com>
2025-07-12 10:43:18 +0900

    chore: workflow 수정 테스트

--- service/posts-read/src/main/java/com/eeum/postsread/PostsReadApplication.java
+++ service/posts-read/src/main/java/com/eeum/postsread/PostsReadApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         "com.eeum.postsread",
         "com.eeum.common.securitycore"
 }
-)
+)//
 public class PostsReadApplication {
 
     public static void main(String[] args) {
