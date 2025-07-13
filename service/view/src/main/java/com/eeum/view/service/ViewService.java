dae5012fde251646eb6b3e69f1d4be124a13ed25
LeeJaeHyeok97 <hazardous10@naver.com>
2025-07-13 19:41:45 +0900

    feat: 조회수 기능 분산락 추가

--- service/view/src/main/java/com/eeum/view/service/ViewService.java
+++ service/view/src/main/java/com/eeum/view/service/ViewService.java
@@ -1,8 +1,11 @@
 package com.eeum.view.service;
 
 import com.eeum.view.repository.ViewCountRepository;
+import com.eeum.view.repository.ViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -10,9 +13,16 @@ public class ViewService {
 
     private final ViewCountRepository viewCountRepository;
     private final ViewCountBackupProcessor viewCountBackupProcessor;
+    private final ViewDistributedLockRepository viewDistributedLockRepository;
+
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long postId, Long userId) {
+        if (!viewDistributedLockRepository.lock(postId, userId, TTL)) {
+            return viewCountRepository.read(postId);
+        }
+
         Long count = viewCountRepository.increase(postId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             viewCountBackupProcessor.backUp(postId, count);
