872b7f0350d59bb6e99498f622d290824cf0c691
LeeJaeHyeok97 <hazardous10@naver.com>
2025-08-25 17:57:44 +0900

    feat: 댓글 갯수 집계 시 낙관적락 적용

--- src/main/java/com/eeum/EEUMApplication.java
+++ src/main/java/com/eeum/EEUMApplication.java
@@ -2,7 +2,10 @@ package com.eeum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class EEUMApplication {
 
